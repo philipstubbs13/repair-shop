8461b7b460d96523fe7644bdaaa52a99a11eb5d0
Phil Stubbs <philipstubbs13@gmail.com>
2025-06-07 19:37:06 -0500

    nextjs repair shop

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Page Not Found",
+};
+
+export default function NotFound() {
+  return (
+    <div className="px-2 w-full">
+      <div className="mx-auto py-4 flex flex-col justify-center items-center gap-4">
+        <h2 className="text-2xl">Page Not Found</h2>
+      </div>
+    </div>
+  );
+}
