ec26423512b9668281523754d4180ba5695d0230
Phil Stubbs <philipstubbs13@gmail.com>
2025-06-14 16:41:02 -0500

    react table

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,4 +1,6 @@
+import CustomerTable from "@/app/(rs)/customers/CustomerTable";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+import * as Sentry from "@sentry/nextjs";
 import CustomerSearch from "./CustomerSearch";
 
 export const metadata = {
@@ -14,12 +16,20 @@ export default async function Customers({
 
   if (!searchText) return <CustomerSearch />;
 
+  const span = Sentry.startInactiveSpan({
+    name: "getCustomerSearchResults-2",
+  });
   const results = await getCustomerSearchResults(searchText);
+  span.end();
 
   return (
     <>
       <CustomerSearch />
-      <p>{JSON.stringify(results)}</p>
+      {results.length ? (
+        <CustomerTable data={results} />
+      ) : (
+        <p className="mt-4">No results found</p>
+      )}
     </>
   );
 }
