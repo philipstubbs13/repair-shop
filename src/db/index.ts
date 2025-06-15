f67206716572dce453863ab72a7c49ebe19afd08
Phil Stubbs <philipstubbs13@gmail.com>
2025-06-15 12:50:40 -0500

    prepare for deployment

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,9 @@ import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 
-config({ path: ".env.local" });
+if (process.env.NODE_ENV === "development") {
+  config({ path: ".env.local" });
+}
 
 const sql = neon(process.env.DATABASE_URL!);
 
